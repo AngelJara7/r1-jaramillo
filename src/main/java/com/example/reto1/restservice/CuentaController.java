67e91588413999c73060d2190a451ea2f4e0151c
Angel <angeljara07@outlook.com>
2023-10-03 06:40:04 -0500

    agregando metodo para actualizar cuenta

--- src/main/java/com/example/reto1/restservice/CuentaController.java
+++ src/main/java/com/example/reto1/restservice/CuentaController.java
@@ -1,14 +1,14 @@
 package com.example.reto1.restservice;
 
-import java.util.Optional;
-
 import com.example.reto1.Cuenta;
 import com.example.reto1.CuentaRepository;
-import com.fasterxml.jackson.core.JsonProcessingException;
+
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-/*import org.springframework.web.bind.annotation.PutMapping;*/
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,30 +22,57 @@ public class CuentaController {
     private CuentaRepository cuentaRepository;
 
 	@RequestMapping(value = "/accounts", method = RequestMethod.GET)
-	public Iterable<Cuenta> Obtener() throws JsonProcessingException{
-        return cuentaRepository.findAll();
+	public ResponseEntity<Object> ObtenerCuentas() {
+        List<Cuenta> cuentas = (List<Cuenta>) cuentaRepository.findAll();
+        try {
+            return new ResponseEntity<>(cuentas, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("No existen registros", HttpStatus.NOT_FOUND);
+        }
     }
     
     @RequestMapping(value = "/accounts/{id}", method = RequestMethod.GET)
-	public Optional<Cuenta> ObtenerCuenta(@PathVariable() Long id){
-        return cuentaRepository.findById(id);
+	public ResponseEntity<Object> ObtenerCuenta(@PathVariable() Long id){
+        try {
+            Cuenta cuenta = cuentaRepository.findById(id).get();
+            return new ResponseEntity<>(cuenta, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("La cuenta no existe", HttpStatus.NOT_FOUND);
+        }
     }
     
     @RequestMapping(value = "/accounts", method = RequestMethod.POST)
-	public Cuenta Crear(@RequestBody Cuenta cuenta){
-        return cuentaRepository.save(cuenta);
+	public ResponseEntity<Object> Crear(@RequestBody Cuenta cuenta){
+        try {
+            Cuenta nuevaCuenta = cuentaRepository.save(cuenta);
+            return new ResponseEntity<>(nuevaCuenta, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Algo salio mal, no se pudo crear la cuenta", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @RequestMapping(value = "/accounts/{id}", method = RequestMethod.PUT)
-	public Cuenta Actualizar(@RequestBody() Cuenta cuenta){        
-        return cuentaRepository.save(cuenta);
+	public ResponseEntity<Object> Actualizar(@RequestBody() Cuenta cuenta, @PathVariable() Long id){
+        try {
+            Cuenta cuentaActual = cuentaRepository.findById(id).get();
+            cuentaActual.saldo = cuenta.saldo;
+            Cuenta cuentaActualizada = cuentaRepository.save(cuentaActual);
+            return new ResponseEntity<>(cuentaActualizada, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("La cuenta no existe", HttpStatus.NOT_FOUND);
+        }
     }
 
     @RequestMapping(value = "/accounts/{id}", method = RequestMethod.DELETE)
-	public Cuenta Eliminar(@PathVariable() Long id){
-        String estado = "desactivada";
-        Cuenta cuenta = cuentaRepository.findById(id).get();
-        cuenta.estadoCuenta = estado;
-        return cuentaRepository.save(cuenta);
+	public ResponseEntity<Object> Eliminar(@PathVariable() Long id){
+        try {
+            final String estado = "desactivada";
+            Cuenta cuenta = cuentaRepository.findById(id).get();
+            cuenta.estadoCuenta = estado;
+            Cuenta cuentaEliminada = cuentaRepository.save(cuenta);
+            return new ResponseEntity<>(cuentaEliminada, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("La cuenta no existe", HttpStatus.NOT_FOUND);
+        }
     }
 }
